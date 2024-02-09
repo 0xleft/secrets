b11572af740e59ccb5cf4396d705b41283dc07d7
left <107749872+0xleft@users.noreply.github.com>
2024-02-09 10:13:49 +0100

    more stuff and windows integration

--- secrets_backend/scanning/main.py
+++ secrets_backend/scanning/main.py
@@ -1,14 +1,25 @@
 from git import Repo
 import scan
+import hashlib
+import os
 
 class Scan():
     pass
 
 def scan_repo(url: str):
-    repo = Repo.clone_from(url, 'temp')
+    repo_hash = hashlib.sha256(url.encode()).hexdigest()
 
-    scan.scan_gitleaks(repo)
+    repo = Repo.clone_from(url, repo_hash)
+    try:
+        scan.scan_gitleaks(repo, url)
+    except:
+        pass
+
     repo.close()
+    if os.name == 'nt':
+        os.system(f"rmdir /s /q {repo_hash}")
+    else:
+        os.system(f"rm -rf {repo_hash}")
 
 if __name__ == '__main__':
-    scan_repo('https://github.com/0xleft/rl_configs')
+    scan_repo('https://github.com/vidaud/seta')
--- secrets_backend/scanning/storage.py
+++ secrets_backend/scanning/storage.py
@@ -1,19 +1,26 @@
 import pymongo
+import json
 
 mongo_client = pymongo.MongoClient('mongodb://localhost:27017/')
 mongo_db = mongo_client['secrets']
 
 class Secret():
-    def __init__(self, repo_name, commit, path, secret):
-        self.repo_name = repo_name
+    def __init__(self, url, commit, path, secret):
+        self.url = url
         self.commit = commit
         self.path = path
         self.secret = secret
 
     def save(self):
         return mongo_db['secrets'].insert_one({
-            'repo_name': self.repo_name,
+            'url': self.url,
             'commit': self.commit,
             'path': self.path,
             'secret': self.secret
         })
+    
+def store(json_data: str, url: str):
+    json_data = json.loads(json_data)
+
+    for secret in json_data:
+        Secret(url, json_data["commit"], json_data['file'], json_data['secret']).save()
