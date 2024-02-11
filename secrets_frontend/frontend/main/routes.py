0bc41f9e50e4baa47ba9ff32891a9ca7558b3a5c
left <107749872+0xleft@users.noreply.github.com>
2024-02-11 19:54:33 +0100

    add regex to serach

--- secrets_frontend/frontend/main/routes.py
+++ secrets_frontend/frontend/main/routes.py
@@ -36,21 +36,21 @@ def search():
 
     query = {}
     if url != "":
-        query["url"] = url
+        query["url"] = {"url": {"$regex": url, "$options": "i"}}
     if commit != "":
-        query["commit"] = commit
+        query["commit"] = {"commit": {"$regex": commit, "$options": "i"}}
     if path != "":
-        query["path"] = path
+        query["path"] = {"path": {"$regex": path, "$options": "i"}}
     if secret != "":
-        query["secret"] = secret
+        query["secret"] = {"secret": {"$regex": secret, "$options": "i"}}
     if match != "":
-        query["match"] = match
+        query["match"] = {"match": {"$regex": match, "$options": "i"}}
     if rule_id != "":
-        query["rule_id"] = rule_id
+        query["rule_id"] = {"rule_id": {"$regex": rule_id, "$options": "i"}}
     if owner != "":
-        query["owner"] = owner
+        query["owner"] = {"owner": {"$regex": owner, "$options": "i"}}
     if date != "":
-        query["date"] = date
+        query["date"] = {"date": {"$regex": date, "$options": "i"}}
 
     page_end = min(math.ceil(db["info"].find_one()["secret_count"] / PER_PAGE_COUNT), MAX_PAGE_COUNT)
     results = db["secrets"].find(query if session.get("logged_in") is not None else {}, limit=PER_PAGE_COUNT, skip=(page-1)*PER_PAGE_COUNT)
